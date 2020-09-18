c96d5789e9b619dac32747dd6144124de739da54
jiazhuang01 <jiazhuang01@baidu.com>
2020-09-18 20:52:51 +0800

    unet and dataloader

---
+++ data_loader.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# filename: data_loader.py
+# brief: load DRIVE dataset and form as tensors
+# author: Jia Zhuang
+# date: 2020-09-18
+
+from PIL import Image
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch import nn
+import torch.nn.functional as F
+import os
+from glob import glob
+
+def load_dataset(rel_path='.'):
+    
+    train_image_files = sorted(glob(os.path.join(rel_path, 'DRIVE/training/images/*.tif')))
+    train_label_files = sorted(glob(os.path.join(rel_path, 'DRIVE/training/1st_manual/*.gif')))
+    train_mask_files = sorted(glob(os.path.join(rel_path, 'DRIVE/training/mask/*.gif')))
+    
+    for i, filename in enumerate(train_image_files):
+        print('[*] adding {}th train image : {}'.format(i + 1, filename))
+        img = Image.open(filename)
+        imgmat = np.array(img).astype('float')
+        imgmat = (imgmat / 255.0 - 0.5) * 2
+        if i == 0:
+            input_tensor = np.expand_dims(imgmat, axis=0)
+        else:
+            tmp = np.expand_dims(imgmat, axis=0)
+            input_tensor = np.concatenate((input_tensor, tmp), axis=0)
+    new_input_tensor = np.moveaxis(input_tensor, 3, 1)
+            
+    for i, filename in enumerate(train_label_files):
+        print('[*] adding {}th train label : {}'.format(i + 1, filename))
+        label = np.array(Image.open(filename))
+        label = label / 255.0
+        if i == 0:
+            label_tensor = np.expand_dims(label, axis=0)
+        else:
+            tmp = np.expand_dims(label, axis=0)
+            label_tensor = np.concatenate((label_tensor, tmp), axis=0)
+    new_label_tensor = np.stack((label_tensor[:,:,:], 1 - label_tensor[:,:,:]), axis=1)
+    
+    for i, filename in enumerate(train_mask_files):
+        print('[*] adding {}th train mask : {}'.format(i+1, filename))
+        mask = np.array(Image.open(filename))
+        mask = mask / 255.0
+        if i == 0:
+            mask_tensor = np.expand_dims(mask, axis=0)
+        else:
+            tmp = np.expand_dims(mask, axis=0)
+            mask_tensor = np.concatenate((mask_tensor, tmp), axis=0)
+    new_mask_tensor = np.stack((mask_tensor[:,:,:], mask_tensor[:,:,:]), axis=1)
+    
+    return new_input_tensor, new_label_tensor, new_mask_tensor
---
+++ unet_model.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# filename: unet_model.py
+# brief: U-net architecture
+# author: Jia Zhuang
+# date: 2020-09-18
+
+from PIL import Image
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch import nn
+import torch.nn.functional as F
+import os
+from glob import glob
+from unet_utils import *
+
+class Unet(nn.Module):
+    """
+    definition of U-net for segmentation
+    """
+    def __init__(self, img_ch, K_class, fch_base=64, isBN=True, isDeconv=True):
+        super(Unet, self).__init__()
+        
+        self.blocks = nn.ModuleList()
+        
+        self.down1 = ConvNoPool(img_ch, fch_base, isBN)
+        self.down2 = ConvPool(fch_base, fch_base * 2, isBN)
+        self.down3 = ConvPool(fch_base * 2, fch_base * 4, isBN)
+        self.down4 = ConvPool(fch_base * 4, fch_base * 8, isBN)
+        
+        self.encoder = ConvPool(fch_base * 8, fch_base * 16, isBN)
+        
+        self.up1 = UpsampleConv(fch_base * 16 , fch_base * 8, isDeconv, isBN)
+        self.up2 = UpsampleConv(fch_base * 8, fch_base * 4, isDeconv, isBN)
+        self.up3 = UpsampleConv(fch_base * 4, fch_base * 2 , isDeconv, isBN)
+        self.up4 = UpsampleConv(fch_base * 2, fch_base, isDeconv, isBN)
+        
+        self.out = ConvOut(fch_base, K_class)
+        
+        self.blocks = nn.ModuleList([self.down1, self.down2, self.down3,\
+                                     self.down4, self.encoder, self.up1, self.up2,\
+                                     self.up3, self.up4, self.out])
+
+    def forward(self, input_):
+        d1 = self.down1(input_)
+        d2 = self.down2(d1)
+        d3 = self.down3(d2)
+        d4 = self.down4(d3)
+        enc = self.encoder(d4)
+        u1 = self.up1(enc, d4)
+        u2 = self.up2(u1, d3)
+        u3 = self.up3(u2, d2)
+        u4 = self.up4(u3, d1)
+        output_ = self.out(u4)
+        return output_
